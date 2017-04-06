8f39f68d1eea13557b3e47e1493ca1a35786495a
Edmar Ramos <eramos34@student.rcc.edu>
2017-04-06 14:25:03 -0700

    Edmar made some changes

--- CIS-17B_Assignment4_Erith-Tandy-Edmar/TransferWindow.cpp
+++ CIS-17B_Assignment4_Erith-Tandy-Edmar/TransferWindow.cpp
@@ -11,4 +11,11 @@ void TransferWindow::transferWin()
         this,
         tr("Transfer Template"),
         tr("not his fault but blame tandy") );
+    //"Transfer Funds" button should open a
+         // dialog displaying "Account to transfer from"
+          //  selection box, "Account to transfer to"
+            //selection box, and a box to fill in an amount
+            //to transfer. This dialog should also have an "Ok"
+            //button and a "Cancel" button
+    //
 }
